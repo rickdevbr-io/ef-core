ffb0ee677d945ec817d3c21ef108a3719141a002
rickdevbr-io <ricardodeveloper21br@gmail.com>
2024-08-26 12:52:46 -0300

    start project

---
+++ Solution1/src/Freelando.Dados/FreelandoContext.cs
@@ -0,0 +1,35 @@
+﻿using Freelando.Modelo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelando.Dados
+{
+    public class FreelandoContext: DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public FreelandoContext(DbContextOptions<FreelandoContext> options) : base(options)
+        {
+        }
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ConnectionStrings:DefaultConnection"));
+            }
+        }
+
+        public DbSet<Candidatura> Candidaturas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Contrato> Contratos { get; set; }
+        public DbSet<Especialidade> Especialidades { get; set; }
+        public DbSet<Profissional> Profissionais { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+    }
+}
---
+++ Solution1/src/Freelando.Modelos/Especialidade.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Freelando.Modelo;
+
+[Table("TB_Especialidades")]
+public class Especialidade
+{
+    [Column("Id_Especialidade")]
+    public Guid Id { get; set; }
+    
+    [Column("DS_Especialidade")]
+    public string? Descricao { get; set; }
+
+}
